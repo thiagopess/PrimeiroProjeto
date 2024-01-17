8ac5f181cafa4b0c811817fb4b1445bcbb55b58c
thiagopess <tpessoa0@gmail.com>
2024-01-17 18:50:20 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+//pegar a tag img
+const img = document.querySelector("#profile img")
+
+//subistituir a imagem
+if(html.classList.contains('light')) {
+    //se tiver light mode, adicionar a imagem light
+img.setAttribute('src', './assets/avatar-light.png')
+}else{
+
+}
+// se não tiver em light mode, manter a imagem normal
+img.setAttribute('src', './assets/avatar.png')
+}
